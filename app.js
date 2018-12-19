dcb0a8d0431853c940416d155fdb8fc1375370a6
hu188 <18851151385@139.com>
2018-12-19 14:50:17 +0800

    fix

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 import { http } from './utils/http'
 App({
   onLaunch(options) {
-    this.queryType()
-    this.checkAuth(res => {
-      const data = this.checkLogin()
-      if (data === '' && this.globalData.auth) {
-        this.login()
-      } else if (this.globalData.auth) {
-        this.globalData.sessionkey = data
-        wx.getUserInfo({
-          success: (res) => {
-            this.globalData.userInfo = res
-          },
-        });
-      }
-    })
+    // this.checkAuth(res => {
+    //   const data = this.checkLogin()
+    //   if (data === '' && this.globalData.auth) {
+    //     this.login()
+    //   } else if (this.globalData.auth) {
+    //     this.globalData.sessionkey = data
+    //     wx.getUserInfo({
+    //       success: (res) => {
+    //         this.globalData.userInfo = res
+    //       },
+    //     });
+    //   }
+    // })
   },
   //测试获取用户的guid
   login() {
@@ -26,28 +25,31 @@ App({
             const data = {
               "jsCode": res.code,
               "type": "1",
-              "storeType": this.globalData.type.type,
-              "storeLevel": this.globalData.type.level,
+              "id": this.globalData.id,
               ...result
             }
-            http('user/login', JSON.stringify(data), 1, 1).then(res => {
-              const { sessionKey, userId } = res
+            
+            http('qsq/service/external/user/login', JSON.stringify(data), 1, 1).then(res => {
+              const { sessionKey, id, isvip } = res
+              this.globalData.userId = id
               this.globalData.sessionkey = sessionKey
+              const { levelTypeId, type } = res;
+               this.globalData.type = { level: levelTypeId, type }
               this.globalData.userInfo = result
               wx.showToast({
                 title: '登录成功！',
               })
-              http('recharge/queryBalance', { sessionKey: sessionKey }, 1).then(res => {
-                const { chargeMoney } = res
+              http('qsq/service/external/recharge/queryBalance', { sessionKey: sessionKey }, 1).then(res => {
+                const { chargeMoney} = res
                 this.globalData.balance = chargeMoney
               })
               wx.setStorage({
                 key: 'sessionKey', // 缓存数据的key
                 data: sessionKey // 要缓存的数据
               });
-              if (!userId) {
-                http('coupon/init', {sessionKey}, 1).then(res => {
-                 console.log(res)
+              if (!id) {
+                http('qsq/service/external/coupon/init', {sessionKey}, 1).then(res => {
+                 //console.log(res)
                 })
               }
             })
@@ -62,19 +64,13 @@ App({
         const { authSetting } = res
         if (authSetting['scope.userInfo']) {
           this.globalData.auth = true
-          callback(0)
         } else {
           this.globalData.auth = false
           callback(1)
         }
-        /*
-         * res.authSetting = {
-         *   "scope.userInfo": true,
-         *   "scope.userLocation": true
-         * }
-         */
       }
     })
+ 
   },
   checkLogin() {
     const data = wx.getStorageSync('sessionKey');
@@ -90,41 +86,7 @@ App({
       });
     })
   },
-  queryType() {
-    return new Promise((resolve, reject) => {
-      http('store/type', { "appId": "wx978041ffe305d125" }).then(res => {
-        const { levelTypeId, type } = res;
-        this.globalData.type = { level: levelTypeId, type }
-        resolve({ level: levelTypeId, type })
-      })
-    })
-  },
-  queryBanlance () {
-    const { sessionkey} = this.globalData
-    http('recharge/queryBalance', { sessionKey: sessionkey }, 1).then(res => {
-      const { chargeMoney } = res
-      this.globalData.balance = chargeMoney
-    })
-  },
-  sendTemplate(formId, order) {
-    const { orderItemList } = order
-    console.log(orderItemList)
-    const sessionKey = getApp().globalData.sessionkey
-    const params = JSON.stringify({
-      sessionKey: sessionKey,
-      templateId: 'W1oIoiPcifWe68y8jigaOgRkVmMFj8Z1BkF0sIRYN0g',
-      keyword1: order.orderNo,
-      keyword2: Number(order.payment).toFixed(2),
-      keyword3: order.createTime,
-      keyword4: orderItemList[0].goodsName,
-      miniProgramUrl: 'pages/order/index',
-      formId: formId,
-      remark: '',
-      first: ''
-    })
-    http('template/send', params, 1, 1).then(res => {
-    })
-  },
+
   globalData: {
     userInfo: null,
     user_id: '',//支付宝userid
@@ -133,14 +95,25 @@ App({
     appsecret: '',
     isGetedAuth: 0,//是否获取到授权
     isCanceledAuth: 0,//是否取消了授权
-    //hostname: 'https://www.tianrenyun.com.cn',
-    hostname: 'https://qsq.mynatapp.cc/api',
+    hostname: 'https://www.tianrenyun.com',
+    //hostname: 'https://qsq.mynatapp.cc/api',
     imgPath: 'https://www.tianrenyun.com/qsqFile/filelib/imagelib/dealerlib/',
     isNeedLogin: 0,//0为不需要登录，1为需要手机号验证码登录
     goodsList: [], //结算时选中的商品,
     type: {},
     sessionkey: '',
     auth: false,
-    balance: 0
+    balance: 0,
+    deviceId:'',
+    userId:'',
+    isVip: '',
+    isFirstBuy:0,
+    id:'',
+    sign:'',
+    deviceName:'',
+    urlType:'',
+    url:'',
+    tp: '',//0单货单，1多货道
+    nickname:''
   },
 });
